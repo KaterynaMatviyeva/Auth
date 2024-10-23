b38e3f734ecfbb1319e9170fbaaaa9481bf9697b
Kateryna <katiaemme96@yahoo.it>
2024-10-23 16:50:37 +0200

    not already done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { provideHttpClient } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, NavBarComponent, FooterComponent],
+  imports: [BrowserModule, AppRoutingModule, NgbModule],
+  providers: [provideHttpClient()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from '../../environments/environment.development';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { BehaviorSubject } from 'rxjs';
+import { iAccessData } from '../interfaces/i-access-data';
+import { iUser } from '../interfaces/i-user';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor(private http: HttpClient, private router: Router) {}
+
+  jwtHelper: JwtHelperService = new JwtHelperService();
+
+  registerUrl: string = environment.registerUrl;
+  loginUrls: string = environment.loginUrl;
+
+  authSubject$ = new BehaviorSubject<iAccessData | null>(null);
+
+  register(newUser: Partial<iUser>) {
+    return this.http.post<iAccessData>(this.registerUrl, newUser);
+  }
+}
